60461bf8e38dcfca833e9ab5c658f4f3a433f3f6
GentritUkelli <112003299+GentritUkelli@users.noreply.github.com>
2023-12-21 14:11:13 +0100

    no message

--- restaurant-point-json/restaurant-point-backend/routes/menus.js
+++ restaurant-point-json/restaurant-point-backend/routes/menus.js
@@ -9,14 +9,14 @@ module.exports = function (server) {
     const restaurantId = parseInt(request.params.id);
     const requestBody = request.body;
     const restaurantsData = router.db.get("restaurants").value();
-    const index = restaurantsData.findIndex((restaurant) => restaurant.id === restaurantId);
+    const index = restaurantsData.findIndex((rest) => rest.id === restaurantId);
 
     // check if the restaurant exists
     if (index === -1) {
       response.status(404).json({ error: "Restaurant not found" });
     } else {
       const restaurant = restaurantsData[index];
-      const menuList = restaurant.menus;
+      const menuList = restaurant.menu_list;
 
       if (requestBody.id === undefined) {
         let menuId;
@@ -24,33 +24,32 @@ module.exports = function (server) {
         const newMenu = {
           id: menuId,
           name: requestBody.name,
-          menu_items: [],
+          menuItems_list: [],
         };
         menuList.push(newMenu);
-        restaurant.menus = menuList;
+        restaurant.menu_list = menuList;
         router.db.set("restaurants", restaurantsData).write();
         const lastUsedId = router.db.get("lastUsedId").value();
         lastUsedId.menuId = menuIdCounter;
         router.db.set("lastUsedId", lastUsedId).write();
         response.json(restaurantsData[index]);
       } else {
-        const menuIndex = restaurant.menus.findIndex(
-          (menu) => menu.id === parseInt(requestBody.id)
+        const menusIndex = restaurant.menu_list.findIndex(
+          (menus) => menus.id === parseInt(requestBody.id)
         );
-
-        if (menuIndex === -1) {
+        if (menusIndex === -1) {
           response
             .status(404)
             .json({ error: "Menu not found in this restaurant" });
         } else {
           requestBody.id = parseInt(requestBody.id);
           const updatedMenu = {
-            id: requestBody.id,
-            name: requestBody.name,
-            menu_items: [],
+            id: menuId,
+          name: requestBody.name,
+          menuItems_list: [],
           };
-          restaurant.menus[menuIndex] = {
-            ...restaurant.menus[menuIndex],
+          restaurant.menu_list[menusIndex] = {
+            ...restaurant.menu_list[menusIndex],
             ...updatedMenu,
           };
           router.db.set("restaurants", restaurantsData).write();
@@ -67,7 +66,7 @@ module.exports = function (server) {
     if (!restaurant) {
       response.status(404).json({ error: "Restaurant not found" });
     } else {
-      const menuList = restaurant.menus;
+      const menuList = restaurant.menu_list;
       response.json(menuList);
     }
   });
@@ -77,18 +76,16 @@ module.exports = function (server) {
     const menuId = parseInt(request.params.id);
     const restaurantsData = router.db.get("restaurants").value();
     const restaurant = restaurantsData.find((rest) => rest.id === restaurantId);
-
     if (!restaurant) {
       response.status(404).json({ error: "Restaurant not found" });
     } else {
-      const menu = restaurant.menus.find(
-        (menu) => menu.id === menuId
+      const menu = restaurant.menu_list.find(
+        (men) => men.id === menuId
       );
-
       if (!menu) {
         response
           .status(404)
-          .json({ error: "Menu not found in this restaurant" });
+          .json({ error: "Menu not found in this Restaurant" });
       } else {
         response.json(menu);
       }
@@ -97,22 +94,19 @@ module.exports = function (server) {
 
   server.delete("/api/menus/delete/:id", (request, response) => {
     const menuId = parseInt(request.params.id);
-    const restaurantData = router.db.get("restaurants").value();
+    const restaurantsData = router.db.get("restaurants").value();
     let menuDeleted = false;
-
-    restaurantData.forEach((restaurant) => {
-      const menuIndex = restaurant.menus.findIndex(
+    restaurantsData.forEach((restaurant) => {
+      const menuIndex = restaurant.menu_list.findIndex(
         (menu) => menu.id === menuId
       );
-
       if (menuIndex !== -1) {
-        restaurant.menus.splice(menuIndex, 1);
+        restaurant.menu_list.splice(menuIndex, 1);
         menuDeleted = true;
       }
     });
-
     if (menuDeleted) {
-      router.db.set("restaurants", restaurantData).write();
+      router.db.set("restaurant", restaurantsData).write();
       response.json({ message: "Menu deleted successfully." });
     } else {
       response
