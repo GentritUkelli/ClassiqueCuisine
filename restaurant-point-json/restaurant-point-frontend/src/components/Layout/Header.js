97c8531ffded8f048935533a5ff6c335714d64d4
GentritUkelli <112003299+GentritUkelli@users.noreply.github.com>
2024-01-01 16:42:12 +0100

    no message

--- restaurant-point-json/restaurant-point-frontend/src/components/Layout/Header.js
+++ restaurant-point-json/restaurant-point-frontend/src/components/Layout/Header.js
@@ -4,26 +4,26 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <div>
-      <nav className="bg-gradient-to-b from-purple-800 via-purple-600 to-blue-500 p-4">
+      <nav className="bg-gradient-to-r from-purple-700 via-purple-600 to-violet-500 p-4">
         <div className="container mx-auto flex justify-between items-center">
-          <Link to="/" className="text-white text-4xl font-extrabold hover:text-gray-300">
+          <Link to="/" className="text-white text-4xl font-extrabold hover:text-amber-400">
             ClassiqueCuisine
           </Link>
 
           <div className="flex space-x-4">
-            <Link to="/restaurants" className="text-white text-xl hover:text-gray-200">
+            <Link to="/restaurants" className="text-white text-xl hover:text-amber-400">
               <span className="border-b-2 border-transparent hover:border-gray-200">
                 Restaurants
               </span>
             </Link>
 
-            <Link to="/menus" className="text-white text-xl hover:text-gray-200">
+            <Link to="/menus" className="text-white text-xl hover:text-amber-400">
               <span className="border-b-2 border-transparent hover:border-gray-200">
                 Menus
               </span>
             </Link>
 
-            <Link to="/menuItems" className="text-white text-xl hover:text-gray-200">
+            <Link to="/menuItems" className="text-white text-xl hover:text-amber-400">
               <span className="border-b-2 border-transparent hover:border-gray-200">
                 Menu Items
               </span>
