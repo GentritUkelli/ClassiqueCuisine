fae0aed75e14310bb83110464afcde726263fc22
GentritUkelli <112003299+GentritUkelli@users.noreply.github.com>
2024-01-30 22:13:35 +0100

    no message

--- restaurant-point-json/restaurant-point-frontend/src/actions/types.js
+++ restaurant-point-json/restaurant-point-frontend/src/actions/types.js
@@ -3,3 +3,13 @@ export const GET_RESTAURANTS = 'GET_RESTAURANTS';
 export const GET_RESTAURANT = 'GET_RESTAURANT';
 export const DELETE_RESTAURANT = 'DELETE_RESTAURANT';
 export const CREATE_RESTAURANT = 'CREATE_RESTAURANT';
+
+export const GET_MENUS = 'GET_MENUS';
+export const GET_MENU = 'GET_MENU';
+export const DELETE_MENU = 'DELETE_MENU';
+export const CREATE_MENU = 'CREATE_MENU';
+
+export const GET_MENU_ITEMS = 'GET_MENU_ITEMS';
+export const GET_MENU_ITEM = 'GET_MENU_ITEM';
+export const DELETE_MENU_ITEM = 'DELETE_MENU_ITEM';
+export const CREATE_MENU_ITEM = 'CREATE_MENU_ITEM';
