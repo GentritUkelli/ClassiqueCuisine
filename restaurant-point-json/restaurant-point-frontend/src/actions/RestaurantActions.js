014d8bc6c071920e9ca475e9bb9496020496f28a
GentritUkelli <112003299+GentritUkelli@users.noreply.github.com>
2024-02-14 18:10:48 +0100

    no message

---
+++ restaurant-point-json/restaurant-point-frontend/src/actions/RestaurantActions.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import axios from "axios";
+import{
+  GET_ERRORS,
+  GET_RESTAURANTS,
+  GET_RESTAURANT,
+  DELETE_RESTAURANT,
+  CREATE_RESTAURANT,
+  GET_MENUS,
+  GET_MENU,
+  DELETE_MENU,
+  CREATE_MENU,
+}from "./types"
